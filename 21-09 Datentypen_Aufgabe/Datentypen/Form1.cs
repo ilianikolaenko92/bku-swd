10d69f06d9304db5e4053c4322bd908dc868d132
Nikolaenko <NIIL02@sms-group.com>
2022-02-22 10:26:34 +0100

    Added 2 SWD tasks for Sep-Okt21

---
+++ 21-09 Datentypen_Aufgabe/Datentypen/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Datentypen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            double a = 30.29; // Gleichzeitige Definition und Initialisierung einer Variable
+            double b = 5.8;
+            double c;
+
+            c = a + b;
+
+            MessageBox.Show($"Das Ergebnis der Addierung lautet: {c}"); // MessageBox.Show() Methode nimmt nur ein String als Argument ein. Auch einfach c.ToString() / Convert.ToString(c) / (String)(c) / .TryParse() ist möglich.
+            MessageBox.Show($"Das Ergebnis der Substaktion lautet: {a - b}"); // eine separate Variable ist für jede Operation in diesem Fall  nicht nötig und nicht Speichereffizient. Ss wäre sinnvoller (wenn überhaupt) einen Delegat einer Funktion mit mehreren Vorgehensweisen für dieselben Argumente zu erstellen
+            MessageBox.Show($"Das Ergebnis der Multiplikation lautet: {a * b}");
+            MessageBox.Show($"Das Ergebnis der Division lautet: {a / b}");
+            MessageBox.Show($"Das Ergebnis des Modulos lautet: {Convert.ToInt32(a % b) }"); // Damit die Modulo immer als ganzzahlige Zahl ausgegeben werden, muss die Ergebnis ins integer konvertiert werden
+
+        }
+    }
+}
---
+++ 21-10 OpenDateiDialog/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //ab hier in einem PAP oder Struktogramm darstellen
+            this.openFileDialog1.InitialDirectory = "C:\\Users\niil02"; //Bitte anpassen - gemacht
+            this.openFileDialog1.Filter = "Excel-Dateien|*.csv";
+            this.openFileDialog1.FileName = "";
+
+            bool datei_gewaehlt=false;
+
+            while ( ! datei_gewaehlt)
+            {
+                if (this.openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    MessageBox.Show("Gewählte Datei: " + this.openFileDialog1.FileName);
+                    datei_gewaehlt = true;
+                }
+                else
+                {
+                    MessageBox.Show("Keine Datei gewählt");
+                }
+            }
+            
+        }
+    }
+}
