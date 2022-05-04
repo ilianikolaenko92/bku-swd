1c46f23a993c69e374d5343d6484a2633c202650
Ilia Nikolaenko <niil02@sms-group.com>
2022-05-04 23:18:01 +0200

    Added an array-based taks for a bitwise IP-validation

---
+++ 22-05 IP-Validierung/IP_Validierung_SWD/IP_Validierung.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace IP_Validierung_SWD
+{
+    partial class IP_Validierung
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.maskedTextBox1_Subnetmask = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox2_ip1 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox3_ip2 = new System.Windows.Forms.MaskedTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2_ip1 = new System.Windows.Forms.Label();
+            this.label3_ip2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1_result = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4_ip2_label = new System.Windows.Forms.Label();
+            this.label3_ip1_label = new System.Windows.Forms.Label();
+            this.label4_result = new System.Windows.Forms.Label();
+            this.label2_subnetmask_label = new System.Windows.Forms.Label();
+            this.listView1_ip1 = new System.Windows.Forms.ListView();
+            this.listView2_ip2 = new System.Windows.Forms.ListView();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // maskedTextBox1_Subnetmask
+            // 
+            this.maskedTextBox1_Subnetmask.Location = new System.Drawing.Point(39, 38);
+            this.maskedTextBox1_Subnetmask.Mask = "099,099,099,099";
+            this.maskedTextBox1_Subnetmask.Name = "maskedTextBox1_Subnetmask";
+            this.maskedTextBox1_Subnetmask.Size = new System.Drawing.Size(265, 22);
+            this.maskedTextBox1_Subnetmask.TabIndex = 0;
+            this.maskedTextBox1_Subnetmask.Text = "2552552550";
+            this.maskedTextBox1_Subnetmask.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.maskedTextBox_MaskInputRejected);
+            this.maskedTextBox1_Subnetmask.Validating += new System.ComponentModel.CancelEventHandler(this.maskedTextBox_Validating);
+            this.maskedTextBox1_Subnetmask.Validated += new System.EventHandler(this.maskedTextBox_Validated);
+            // 
+            // maskedTextBox2_ip1
+            // 
+            this.maskedTextBox2_ip1.Location = new System.Drawing.Point(6, 54);
+            this.maskedTextBox2_ip1.Mask = "099,099,099,099";
+            this.maskedTextBox2_ip1.Name = "maskedTextBox2_ip1";
+            this.maskedTextBox2_ip1.Size = new System.Drawing.Size(265, 22);
+            this.maskedTextBox2_ip1.TabIndex = 1;
+            this.maskedTextBox2_ip1.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.maskedTextBox_MaskInputRejected);
+            this.maskedTextBox2_ip1.Validating += new System.ComponentModel.CancelEventHandler(this.maskedTextBox_Validating);
+            this.maskedTextBox2_ip1.Validated += new System.EventHandler(this.maskedTextBox_Validated);
+            // 
+            // maskedTextBox3_ip2
+            // 
+            this.maskedTextBox3_ip2.Location = new System.Drawing.Point(6, 124);
+            this.maskedTextBox3_ip2.Mask = "099,099,099,099";
+            this.maskedTextBox3_ip2.Name = "maskedTextBox3_ip2";
+            this.maskedTextBox3_ip2.Size = new System.Drawing.Size(265, 22);
+            this.maskedTextBox3_ip2.TabIndex = 2;
+            this.maskedTextBox3_ip2.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.maskedTextBox_MaskInputRejected);
+            this.maskedTextBox3_ip2.Validating += new System.ComponentModel.CancelEventHandler(this.maskedTextBox_Validating);
+            this.maskedTextBox3_ip2.Validated += new System.EventHandler(this.maskedTextBox_Validated);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Subnetmaske";
+            // 
+            // label2_ip1
+            // 
+            this.label2_ip1.AutoSize = true;
+            this.label2_ip1.Location = new System.Drawing.Point(6, 34);
+            this.label2_ip1.Name = "label2_ip1";
+            this.label2_ip1.Size = new System.Drawing.Size(89, 17);
+            this.label2_ip1.TabIndex = 4;
+            this.label2_ip1.Text = "IP-Adresse 1";
+            // 
+            // label3_ip2
+            // 
+            this.label3_ip2.AutoSize = true;
+            this.label3_ip2.Location = new System.Drawing.Point(6, 104);
+            this.label3_ip2.Name = "label3_ip2";
+            this.label3_ip2.Size = new System.Drawing.Size(89, 17);
+            this.label3_ip2.TabIndex = 5;
+            this.label3_ip2.Text = "IP-Adresse 2";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 192);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(170, 48);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "IP-Adressen validieren";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1_result
+            // 
+            this.textBox1_result.Location = new System.Drawing.Point(9, 283);
+            this.textBox1_result.Name = "textBox1_result";
+            this.textBox1_result.Size = new System.Drawing.Size(604, 22);
+            this.textBox1_result.TabIndex = 7;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listView2_ip2);
+            this.groupBox1.Controls.Add(this.listView1_ip1);
+            this.groupBox1.Controls.Add(this.label4_ip2_label);
+            this.groupBox1.Controls.Add(this.label3_ip1_label);
+            this.groupBox1.Controls.Add(this.label4_result);
+            this.groupBox1.Controls.Add(this.maskedTextBox2_ip1);
+            this.groupBox1.Controls.Add(this.textBox1_result);
+            this.groupBox1.Controls.Add(this.label2_ip1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.maskedTextBox3_ip2);
+            this.groupBox1.Controls.Add(this.label3_ip2);
+            this.groupBox1.Location = new System.Drawing.Point(39, 96);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(633, 325);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Geben Sie die IP-Adressen zur Validierung mit der Subnetmaske ein";
+            // 
+            // label4_ip2_label
+            // 
+            this.label4_ip2_label.AutoSize = true;
+            this.label4_ip2_label.Location = new System.Drawing.Point(6, 149);
+            this.label4_ip2_label.Name = "label4_ip2_label";
+            this.label4_ip2_label.Size = new System.Drawing.Size(30, 17);
+            this.label4_ip2_label.TabIndex = 10;
+            this.label4_ip2_label.Text = "null";
+            this.label4_ip2_label.Visible = false;
+            // 
+            // label3_ip1_label
+            // 
+            this.label3_ip1_label.AutoSize = true;
+            this.label3_ip1_label.Location = new System.Drawing.Point(6, 79);
+            this.label3_ip1_label.Name = "label3_ip1_label";
+            this.label3_ip1_label.Size = new System.Drawing.Size(30, 17);
+            this.label3_ip1_label.TabIndex = 9;
+            this.label3_ip1_label.Text = "null";
+            this.label3_ip1_label.Visible = false;
+            // 
+            // label4_result
+            // 
+            this.label4_result.AutoSize = true;
+            this.label4_result.Location = new System.Drawing.Point(6, 263);
+            this.label4_result.Name = "label4_result";
+            this.label4_result.Size = new System.Drawing.Size(141, 17);
+            this.label4_result.TabIndex = 8;
+            this.label4_result.Text = "Validierungsergebnis";
+            // 
+            // label2_subnetmask_label
+            // 
+            this.label2_subnetmask_label.AutoSize = true;
+            this.label2_subnetmask_label.Location = new System.Drawing.Point(42, 63);
+            this.label2_subnetmask_label.Name = "label2_subnetmask_label";
+            this.label2_subnetmask_label.Size = new System.Drawing.Size(30, 17);
+            this.label2_subnetmask_label.TabIndex = 9;
+            this.label2_subnetmask_label.Text = "null";
+            this.label2_subnetmask_label.Visible = false;
+            // 
+            // listView1_ip1
+            // 
+            this.listView1_ip1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.listView1_ip1.HideSelection = false;
+            this.listView1_ip1.Location = new System.Drawing.Point(289, 54);
+            this.listView1_ip1.Name = "listView1_ip1";
+            this.listView1_ip1.Size = new System.Drawing.Size(324, 60);
+            this.listView1_ip1.TabIndex = 11;
+            this.listView1_ip1.UseCompatibleStateImageBehavior = false;
+            this.listView1_ip1.View = System.Windows.Forms.View.List;
+            // 
+            // listView2_ip2
+            // 
+            this.listView2_ip2.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.listView2_ip2.HideSelection = false;
+            this.listView2_ip2.Location = new System.Drawing.Point(289, 124);
+            this.listView2_ip2.Name = "listView2_ip2";
+            this.listView2_ip2.Size = new System.Drawing.Size(324, 60);
+            this.listView2_ip2.TabIndex = 12;
+            this.listView2_ip2.UseCompatibleStateImageBehavior = false;
+            this.listView2_ip2.View = System.Windows.Forms.View.List;
+            // 
+            // IP_Validierung
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 479);
+            this.Controls.Add(this.label2_subnetmask_label);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.maskedTextBox1_Subnetmask);
+            this.Name = "IP_Validierung";
+            this.Text = "IP-Adressen Validator 2022";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1_Subnetmask;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox2_ip1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox3_ip2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2_ip1;
+        private System.Windows.Forms.Label label3_ip2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1_result;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4_result;
+        private System.Windows.Forms.Label label3_ip1_label;
+        private System.Windows.Forms.Label label2_subnetmask_label;
+        private System.Windows.Forms.Label label4_ip2_label;
+        private System.Windows.Forms.ListView listView2_ip2;
+        private System.Windows.Forms.ListView listView1_ip1;
+    }
+}
+
---
+++ 22-05 IP-Validierung/IP_Validierung_SWD/IP_Validierung.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IP_Validierung_SWD
+{
+    public partial class IP_Validierung : Form
+    {
+        private string[] separator_arr = new string[] { "." };
+        private int[] subnet_arr = new int[4] { 0, 0, 0, 0 };
+        private int[] ip1_arr = new int[4] { 0, 0, 0, 0};
+        private int[] ip2_arr = new int[4] { 0, 0, 0, 0 };
+        private int bit_wise_sum_ip1 = 0;
+        private int bit_wise_sum_ip2 = 0;
+        private bool isMatched = false;
+
+
+        public IP_Validierung()
+        {
+            InitializeComponent();
+        }
+
+
+        /* Area of standard WinForms event-based methods */
+
+        private void maskedTextBox_Validating(object sender, CancelEventArgs e)
+        /* Checks if the received value of an MaskedTextBox matches the requirement for each segment to be between 0 and 255
+         Updates message labels and colors according to the result of the validation
+         In case of successful validation:
+         Cleares a corresponding ListView element of any previous items
+         Fills a corresponding class array with segments 
+         Adds each segment as an element of a correspong ListView element
+         */
+            
+            // Validating of a MaskedTextBox is fired just before the validation process starts, this is the place to put the actual validation code.
+        {
+
+            if (sender == maskedTextBox1_Subnetmask)
+            {
+                ValidateInputAndFillIntArray(maskedTextBox1_Subnetmask, ref subnet_arr, label2_subnetmask_label, ref e);
+            }
+            else if (sender == maskedTextBox2_ip1)
+            {
+                listView1_ip1.Items.Clear();
+
+                ValidateInputAndFillIntArray(maskedTextBox2_ip1, ref ip1_arr, label3_ip1_label, ref e);
+
+                for (int index = 0; index < ip1_arr.Length; index++)
+                {
+                    DisplayArraySegment(ref maskedTextBox2_ip1, ip1_arr[index], index + 1);
+                }
+            }
+            else if (sender == maskedTextBox3_ip2)
+            {
+                listView2_ip2.Items.Clear();
+
+                ValidateInputAndFillIntArray(maskedTextBox3_ip2, ref ip2_arr, label4_ip2_label, ref e);
+
+                for (int index = 0; index < ip2_arr.Length; index++)
+                {
+                    DisplayArraySegment(ref maskedTextBox3_ip2, ip2_arr[index], index + 1);
+                }
+            }
+        }
+
+        private void maskedTextBox_Validated(object sender, EventArgs e)
+        /* Checks if all 3 class arrays are not filled with default values (i.e. all three inputs were validated right) and reactivates the button 
+         */
+
+            // Validated is fired after the validation process of the MaskedTextBox element has finished and is designed to be the place to something based on the validation result.
+        {
+            Console.WriteLine("Validation succeeded");
+            if (!CheckStandardValues(subnet_arr) && !CheckStandardValues(ip1_arr) && !CheckStandardValues(ip2_arr))
+            {
+                button1.Enabled = true;
+            }
+        }
+
+        private void maskedTextBox_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        /* Processes an event of any type mismatch of an input to a numeric Mask of a MaskedTextBox item
+         */
+        {
+            Console.WriteLine("Only numbers are allowed!");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+         /* 
+         Checks if all 3 class array vars are filled with validated segments
+         Performs the bit-wise addition of segments for each of 2 IP-adresses with the subnet mask
+         Updates the output text field with a corresponding message at all stages of validation
+         */
+        {
+            if ( !CheckStandardValues(subnet_arr) && !CheckStandardValues(ip1_arr) && !CheckStandardValues(ip2_arr))
+            {
+                textBox1_result.Text = "Inputs are validated successfully! Ready to validate the IP-adresses";
+                textBox1_result.ForeColor = Color.Green;
+
+                bit_wise_sum_ip1 = ValidateNetworkAdress(subnet_arr, ip1_arr);
+                bit_wise_sum_ip2 = ValidateNetworkAdress(subnet_arr, ip2_arr);
+
+                isMatched = bit_wise_sum_ip1 == bit_wise_sum_ip2 ? true : false;
+            }
+            else
+            {
+                textBox1_result.Text = "The subnet mask or one of IP-adresses are not filled correctly!";
+                textBox1_result.ForeColor = Color.Red;
+
+                isMatched = false;
+            }
+
+            if (isMatched)
+            {
+                textBox1_result.Text = $"Network adresses match! {bit_wise_sum_ip1}";
+                textBox1_result.ForeColor = Color.Green;
+            }
+            else
+            {
+                textBox1_result.Text = $"Network adresses do not match! {bit_wise_sum_ip1} and {bit_wise_sum_ip2}";
+                textBox1_result.ForeColor = Color.Red;
+            }
+        }
+
+
+        /* Area of custom logic methods */
+
+        private void ValidateInputAndFillIntArray(MaskedTextBox masked_input, ref int[] num_arr, Label info_label, ref CancelEventArgs validation_event)
+        /* Splits a string value of the input in a MaskedTextBox into a str-array using a separator from the class array and removes any empty symbols
+         * Checks that the str-array contains 4 elements (segments)
+         * If all segments are in place, converts all items of the str-array into an int-array
+         * For all items of the int-array validates their value and if all segments have a correct value copies them to the corresponding class array of the MaskedTextBox element
+         * Updates other related elements (labels, styles) on the run
+         * Cancels validation event if anything goes wrong with the validation. See more @ https://docs.microsoft.com/en-us/dotnet/desktop/winforms/user-input-validation-in-windows-forms?view=netframeworkdesktop-4.8&redirectedfrom=MSDN    
+        */
+        {
+            String[] split_str_arr = masked_input.Text.Split(separator_arr, StringSplitOptions.RemoveEmptyEntries);
+
+            if (split_str_arr.Length == 4)
+            {
+                int[] split_int_arr = Array.ConvertAll(split_str_arr, str_seg => Convert.ToInt32(str_seg));
+
+                if (Array.TrueForAll(split_int_arr, CheckSegments))
+                {
+                    System.Array.Copy(split_int_arr, num_arr, 4);
+
+                    masked_input.ForeColor = Color.Black;
+                    info_label.Text = "All segments are correct!";
+                    info_label.ForeColor = Color.Green;
+                    info_label.Visible = true;
+                }
+
+                else
+                {
+                    masked_input.ForeColor = Color.Red;
+                    info_label.Text = "One of the segments is wrong! Must be between 0 and 255";
+                    info_label.ForeColor = Color.Red;
+                    info_label.Visible = true;
+                    button1.Enabled = false;
+
+                    System.Array.Clear(num_arr, 0, num_arr.Length);
+                    textBox1_result.Text = "";
+                    validation_event.Cancel = true;
+                }
+
+            }
+            else
+            {
+                masked_input.ForeColor = Color.Red;
+                info_label.Text = "4 Segments must be present!";
+                info_label.ForeColor = Color.Red;
+                info_label.Visible = true;
+
+                button1.Enabled = false;
+                textBox1_result.Text = "";
+
+                validation_event.Cancel = true;
+            }
+        }
+
+        private void DisplayArraySegment(ref MaskedTextBox masked_input, int segment, int segment_num)
+        /* Adds a new item to the ListView item with a value of an int segment received from a class array
+         */
+        {
+            if (masked_input == maskedTextBox2_ip1)
+            {
+                listView1_ip1.Items.Add($"Segment {segment_num}: {segment}");
+            }
+            else if (masked_input == maskedTextBox3_ip2)
+            {
+                listView2_ip2.Items.Add($"Segment {segment_num}: {segment}");
+            }
+        }
+
+        private int ValidateNetworkAdress(int[] submask_arr, int[] ip_arr)
+        /* Performs a bit-wise addition of each segment with the same index of the class arrays of a subnet mask and of an ip-address
+         * returns:
+         *  an int of a bit-wise sum of all segments
+         
+        More about bit-wise operations: https://www.tutorialspoint.com/csharp/csharp_bitwise_operators.htm
+         */
+        {
+            int bit_wise_sum = 0;
+
+            for (int index = 0; index < submask_arr.Length; index++ )
+            {
+                bit_wise_sum += submask_arr[index] & ip_arr[index];
+            }
+
+            return bit_wise_sum;
+        }
+
+        /* Static helper functions */
+
+        private static bool CheckSegments(int segment)
+        /* Returns a bool for a check, if a segment value lies between 0 and 255 */
+        {
+            return 0 <= segment && segment <= 255;
+        }
+
+        private static bool CheckStandardValues(int[] int_array)
+        /* Returns a bool for a check, if all items of an array contain standard values of their type (0 for integers) */
+        {
+            return int_array.All(x => x == default(int));
+        }
+    }
+}
