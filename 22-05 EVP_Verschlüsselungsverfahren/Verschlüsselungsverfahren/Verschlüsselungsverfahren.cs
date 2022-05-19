7bbeb272be56690e255001afa267252324231094
Ilia Nikolaenko <niil02@sms-group.com>
2022-05-19 17:12:00 +0200

    EVP Encodings task - fixed logic of working with negative indexes

--- 22-05 EVP_Verschlüsselungsverfahren/Verschlüsselungsverfahren/Verschlüsselungsverfahren.cs
+++ 22-05 EVP_Verschlüsselungsverfahren/Verschlüsselungsverfahren/Verschlüsselungsverfahren.cs
@@ -17,18 +17,20 @@ namespace Verschlüsselungsverfahren
             string vigenere_schlüsselwort = "sicher";
 
             string klartext = "einen guten start";
+            string verschlüsselt_cäsar = "lfk elq hlq vlfkhuhu vdwc";
+            string verschlüsselt_vigenere = "ctcbwlj dqy avapphgylmp";
 
             Console.WriteLine($"Ergebnis der Verschlüsselung mit dem Cäsar-Verfahrens und dem Schlüssel {cäsar_schlüssel}:");
             CalculateCäsar(zeichensatz, cäsar_schlüssel, klartext, "encode");
 
             Console.WriteLine($"Ergebnis der Entschlüsselung mit dem Cäsar-Verfahrens und dem Schlüssel {cäsar_schlüssel}:");
-            CalculateCäsar(zeichensatz, cäsar_schlüssel, klartext, "decode");
+            CalculateCäsar(zeichensatz, cäsar_schlüssel, verschlüsselt_cäsar, "decode");
 
             Console.WriteLine($"Ergebnis der Verschlüsselung mit dem Vigenère-Verfahrens und dem Schlüsselwort -{vigenere_schlüsselwort}-:");
             CalculateVigenere(zeichensatz, vigenere_schlüsselwort, klartext, "encode");
 
             Console.WriteLine($"Ergebnis der Entschlüsselung mit dem Vigenère-Verfahrens und dem Schlüsselwort -{vigenere_schlüsselwort}-:");
-            CalculateVigenere(zeichensatz, vigenere_schlüsselwort, klartext, "decode");
+            CalculateVigenere(zeichensatz, vigenere_schlüsselwort, verschlüsselt_vigenere, "decode");
 
             Console.ReadLine();
         }
@@ -51,8 +53,8 @@ namespace Verschlüsselungsverfahren
                 }
                 else if (operation == "decode")
                 {
-                    int encrypted_idx = (Math.Abs(alphabet.IndexOf(message[i].ToString()) - key)) % 26;
-                    result_text += alphabet[encrypted_idx];
+                    int encrypted_idx = (alphabet.IndexOf(message[i].ToString()) - key) % 26;
+                    result_text += (encrypted_idx < 0) ? alphabet[26 + encrypted_idx] : alphabet[encrypted_idx];  // in case of a negative index ADDS it to the length of the list of letters (i.e. reduces 26 by the negative index) to reach a correct letter backwards
                 }
 
             };
@@ -65,7 +67,7 @@ namespace Verschlüsselungsverfahren
         {
             int codeword_len = codeword.Length;
             string result_text = "";
-            int skips_counter = 0;  // is used to address a correct index in a codeword when iterating using -i- through a message string with symbols that are not in the list of letters (" " etc.): their indexes should be skipped since the algorythm only applies to letters in the message
+            int skips_counter = 0;  // is used to address a correct index in a codeword when iterating using -i- through a message string with symbols that are not in the list of letters (" " etc.): their indexes should be skipped since the algorythm only applies to letters in the message 
 
             for (int i = 0; i < message.Length; i++)
             {
@@ -85,7 +87,8 @@ namespace Verschlüsselungsverfahren
                     }
                     else if (operation == "decode")
                     {
-                        result_text += alphabet[Math.Abs(temp_idx_mssg - temp_idx_codeword) % 26];
+                        int decoding_idx = temp_idx_mssg - temp_idx_codeword;
+                        result_text += (decoding_idx < 0) ? alphabet[26 + decoding_idx] : alphabet[(decoding_idx) % 26];  // in case of a negative index ADDS it to the length of the list of letters (i.e. reduces 26 by the negative index) to reach a correct letter backwards
                     }
                 }
             }
